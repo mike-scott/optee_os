nxp-imx/imx-optee-os

imx_pl310 add pl310_enabled API
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/imx_pl310.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -55,6 +56,11 @@ void arm_cl2_config(vaddr_t pl310_base)
 
 	/* invalidate all cache ways */
 	arm_cl2_invbyway(pl310_base);
+}
+
+bool pl310_enabled(vaddr_t pl310_base)
+{
+	return read32(pl310_base + PL310_CTRL) & 1;
 }
 
 void arm_cl2_enable(vaddr_t pl310_base)

mike-scott/optee_os

core/arch/arm/plat-imx/imx_pl310.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -36,6 +37,11 @@ void arm_cl2_config(vaddr_t pl310_base)
 
 	/* invalidate all cache ways */
 	arm_cl2_invbyway(pl310_base);
+}
+
+bool pl310_enabled(vaddr_t pl310_base)
+{
+	return io_read32(pl310_base + PL310_CTRL) & 1;
 }
 
 void arm_cl2_enable(vaddr_t pl310_base)